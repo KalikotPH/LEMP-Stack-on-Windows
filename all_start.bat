37115a68c7b384ba973c61edfb3da477bf696894
Bytes Crafter <bytescrafter@gmail.com>
2020-04-13 11:35:21 +0800

    Change the port of php used.

--- all_start.bat
+++ all_start.bat
@@ -5,7 +5,7 @@ start ./nginx.exe
 
 echo STARTING PHP...
 set PATH=./php;%PATH%
-start ./tools/RunHiddenConsole.exe ./php/php-cgi.exe -b 127.0.0.1:9123
+start ./tools/RunHiddenConsole.exe ./php/php-cgi.exe -b 127.0.0.1:9000
 
 echo STARTING MYSQL...
 start ./tools/RunHiddenConsole.exe ./mysql/bin/mysqld --defaults-file=mysql\bin\my.ini --standalone --console
--- restart_php.bat
+++ restart_php.bat
@@ -5,6 +5,6 @@ taskkill /f /im php-cgi.exe
 
 echo STARTING PHP...
 set PATH=./php;%PATH%
-start ./tools/RunHiddenConsole.exe ./php/php-cgi.exe -b 127.0.0.1:9123
+start ./tools/RunHiddenConsole.exe ./php/php-cgi.exe -b 127.0.0.1:9000
 
 PAUSE
